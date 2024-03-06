6af12fa9eca280139bfb5e046e07dd9093d7d91c
Habib Julio <apple@Apples-iMac.local>
2024-03-07 10:02:24 +1030

    5th sprint

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+let editButton = document.querySelector(".author__info-button");
+
+function showEditInfo() {
+  let editButton = document.querySelector(".popup");
+  editButton.setAttribute("style", "display: flex");
+}
+
+editButton.addEventListener("click", showEditInfo);
+
+let closeButton = document.querySelector(".pop__container-closebuttton");
+
+function closeEditForm() {
+  let formWindow = document.querySelector(".popup");
+  formWindow.setAttribute("style", "display: none");
+}
+
+closeButton.addEventListener("click", closeEditForm);
+
+let formElement = document.querySelector(".popup__container");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector(".pop__container-name");
+  let jobInput = document.querySelector(".pop__container-job");
+
+  let newName = nameInput.value;
+  let newJob = jobInput.value;
+
+  let currentName = document.querySelector(".author__info-name");
+  let currentJob = document.querySelector(".author__info-job");
+
+  currentName.textContent = newName;
+  currentJob.textContent = newJob;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+formElement.addEventListener("submit", closeEditForm);
